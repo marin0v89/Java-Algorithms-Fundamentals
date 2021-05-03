a3147ec14e6adad56c03749b14f7623125cc1c5d
marin0v89 <xandarous89@gmail.com>
2021-05-03 10:32:26 +0300

    Java Algorithms - Graph Theory, Traversal and Shortest Paths  - problem 1,2

--- src/l05_graphTheoryTraversalAndShortestPaths/main/java/Main.java
+++ src/l05_graphTheoryTraversalAndShortestPaths/main/java/Main.java
@@ -1,17 +1,45 @@
 package l05_graphTheoryTraversalAndShortestPaths.main.java;
 
-import java.util.Collection;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static boolean[] visited;
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String nextLine = scan.nextLine();
+            if (nextLine.trim().equals("")){
+                graph.add(new ArrayList<>());
+            }else {
+                List<Integer> nextNodes = Arrays.stream(nextLine.split("\\s+"))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                graph.add(nextNodes);
+            }
+        }
+        System.out.println();
     }
 
     public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
-        throw new AssertionError("Not Implemented");
+        List<Deque<Integer>> components = new ArrayList<>();
+        visited = new boolean[graph.size()];
+
+        for (int start = 0; start < graph.size(); start++) {
+            if (!visited[start]) {
+                Deque<Integer> current = new ArrayDeque<>();
+                //dfs(start, graph, current);
+                components.add(current);
+            }
+        }
+
+
+        return components;
     }
 
     public static Collection<String> topSort(Map<String, List<String>> graph) {
